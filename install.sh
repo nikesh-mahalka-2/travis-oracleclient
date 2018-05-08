967e7533bfaea217aeff6ea788f07edcc194540b
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-08 15:04:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,15 @@
 #!/bin/sh -e
 
-wget https://raw.githubusercontent.com/sriman-biarca/travis-oracleclient/master/packages/oracle-instantclient11.2-basic_11.2.0.4.0-2_amd64.deb
-wget https://raw.githubusercontent.com/sriman-biarca/travis-oracleclient/master/packages/oracle-instantclient11.2-devel_11.2.0.4.0-2_amd64.deb
-wget https://raw.githubusercontent.com/sriman-biarca/travis-oracleclient/master/packages/oracle-instantclient11.2-sqlplus_11.2.0.4.0-2_amd64.deb
+#wget https://raw.githubusercontent.com/sriman-biarca/travis-oracleclient/master/packages/oracle-instantclient11.2-basic_11.2.0.4.0-2_amd64.deb
+#wget https://raw.githubusercontent.com/sriman-biarca/travis-oracleclient/master/packages/oracle-instantclient11.2-devel_11.2.0.4.0-2_amd64.deb
+#wget https://raw.githubusercontent.com/sriman-biarca/travis-oracleclient/master/packages/oracle-instantclient11.2-sqlplus_11.2.0.4.0-2_amd64.deb
 
 sudo apt-get install libaio1 -y
 
-sudo dpkg --install oracle-instantclient11.2-devel_11.2.0.4.0-2_amd64.deb
-sudo dpkg --install oracle-instantclient11.2-sqlplus_11.2.0.4.0-2_amd64.deb
-sudo dpkg --install oracle-instantclient11.2-basic_11.2.0.4.0-2_amd64.deb
+sudo dpkg --install ./packages/oracle-instantclient11.2-devel_11.2.0.4.0-2_amd64.deb
+sudo dpkg --install ./packages/oracle-instantclient11.2-sqlplus_11.2.0.4.0-2_amd64.deb
+sudo dpkg --install ./packages/oracle-instantclient11.2-basic_11.2.0.4.0-2_amd64.deb
 export LD_LIBRARY_PATH=/usr/lib/oracle/11.2/client64/lib/:$LD_LIBRARY_PATH
 export ORACLE_HOME=/usr/lib/oracle/11.2/client64
 export PATH=$PATH:$ORACLE_HOME/bin
-
-
 "$ORACLE_HOME/bin/sqlplus" SYSTEM/biarca123@"104.197.220.94/XE" @'sample.txt'
